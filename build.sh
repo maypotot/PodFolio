f6337002950338068c044ac3206c61c34db04966
baamurao <111432334+baamurao@users.noreply.github.com>
2026-03-24 20:18:42 +0800

    Add requirements.txt

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-pip install django
-pip install psycopg2-binary
-pip install dj-database-url
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
 
 python src/podfolio_db_backend/manage.py makemigrations
 python src/podfolio_db_backend/manage.py migrate
