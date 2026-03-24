4221648b4d04fef7d38ebafd3e61deab05da1ca7
baamurao <111432334+baamurao@users.noreply.github.com>
2026-03-24 20:07:41 +0800

    modified build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ pip install django
 pip install psycopg2-binary
 pip install dj-database-url
 
-python src\podfolio_db_backend\manage.py makemigrations
-python src\podfolio_db_backend\manage.py migrate
+python src/podfolio_db_backend/manage.py makemigrations
+python src/podfolio_db_backend/manage.py migrate
